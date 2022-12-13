9149951c029a9d46994b0875b60f79ab02e292ee
maitelarrarte <81735912+maitelarrarte@users.noreply.github.com>
2022-12-13 15:47:00 +0100

    Mekanikoa kodea
    Eskuineko ta ezkerreko motorran kodea pinen bidez

---
+++ RemoTv.ino
@@ -0,0 +1,147 @@
+// Motor A connections
+int enA = 11; //ABIADURA
+int in1 = 12; //AURREA
+int in2 = 13; //ATZEA
+// Motor B connections
+int enB = 10;
+int in3 = 8;
+int in4 = 7;
+
+
+int incomingByte = 0; // for incoming serial data INPUT
+
+
+void setup() {
+
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+
+  // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  
+  pinMode(22, OUTPUT); // añadido
+  pinMode(23, OUTPUT); // añadido
+  pinMode(24, OUTPUT); // añadido
+
+   digitalWrite(22, HIGH); // añadido
+   digitalWrite(23, HIGH); // añadido
+   digitalWrite(24, HIGH); // añadido
+  
+  // Turn off motors - Initial state
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+
+
+void loop() {
+
+   if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    
+  // Set motors to maximum speed
+  // For PWM maximum possible values are 0 to 255
+  analogWrite(enA, 255);
+  analogWrite(enB, 255);
+
+  
+  if (incomingByte == 'f') {  // añadido en lugar de 8
+  // Forward A & B & C & D
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+  delay(10);
+  digitalWrite(22, LOW); // añadido
+  delay(3000); // añadido
+  }
+
+   if (incomingByte == 'b') {
+  // Backwards A & B & C & D
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  
+  delay(10);
+  digitalWrite(23, LOW); // añadido
+  delay(3000); // añadido
+  }
+
+   if (incomingByte == 'r') {
+  // Left A & B & C & D
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  
+  delay(10);
+  digitalWrite(24, LOW); // añadido
+  delay(3000); // añadido
+  }
+
+  if (incomingByte == 'l') {
+  // Right A & B & C & D
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  
+  delay(10);
+  }
+
+  if (incomingByte == '5') {
+  // Turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+
+  delay(10);
+  }
+   }}
+/*
+//UNEDITED
+// This function lets you control speed of the motors
+void speedControl() {
+  // Turn on motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  
+  // Accelerate from zero to maximum speed
+  for (int i = 0; i < 256; i++) {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
+  
+  // Decelerate from maximum speed to zero
+  for (int i = 255; i >= 0; --i) {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
+  
+  // Now turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}*/
